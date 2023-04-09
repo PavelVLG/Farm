1311eb8f0d729181b63de4143ade3317322a2c84
Pavel <Pavelvlg444@gmail.com>
2023-04-09 19:28:48 +0300

    ADD:(loader => spine)

--- src/scripts/scene/Boot.ts
+++ src/scripts/scene/Boot.ts
@@ -1,4 +1,5 @@
 import Png from 'assets/png.json';
+import Spine from 'assets/spine.json';
 import Sprite from 'assets/atlas.json';
 import { SCENES } from '../util/global';
 import LoadingProgress from './Boot/LoadingProgress';
@@ -17,11 +18,17 @@ export default class Boot extends Phaser.Scene {
     public preload() {
         this.loadAtlas();
         this.loadPng();
+        this.loadSpine();
         this.load.on('progress', (value: number) => this.loadProgress(value));
 
         this.load.on('complete', () => this.changeScene(SCENES.GAME));
     }
 
+    private loadSpine() {
+        Spine.forEach(({ key, json, atlas }) => {
+            this.load.spine(key, json, atlas);
+        });
+    }
     private loadPng() {
         Png.forEach(({ key, path }) => {
             this.load.image(key, path);
